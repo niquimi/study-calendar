ced443677df87bb97f88e802e2ecbbe67c0d292b
niquimi <nico@MacBook-Air-de-Nico.local>
2025-02-25 01:07:53 +0100

    feat: added app structure

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'))
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
